76aec44df67425573963a7a5c201b3b20b5ca7bf
yinghuiSUN <104349614+yinghuiSUN@users.noreply.github.com>
2022-05-06 16:18:07 +0200

    modification | filtrer ajouté dans departement

--- src/DepartementList.js
+++ src/DepartementList.js
@@ -4,8 +4,10 @@ class DeppartmentsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      inputSearcheValue: "",
       data: []
     };
+    this.handleChangeInput = this.handleChangeInput.bind(this);
   }
 
   componentDidMount() {
@@ -17,20 +19,47 @@ class DeppartmentsList extends React.Component {
         });
       });
   }
+
+  //fonction handleChangeInput
+  handleChangeInput(e) {
+    const { value } = e.target;
+    this.setState({
+      inputSearcheValue: value
+    });
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, inputSearcheValue } = this.state;
+    const departementList = data.filter(
+      (dep) => dep.nom.toLowerCase().indexOf(inputSearcheValue) !== -1
+    );
 
     return (
       <div className="App">
         <h1> liste des départements </h1>
-        <table className="liste_dpt">
+
+        <div className="recherche_departement">
+          <form className="filter_departement">
+            <label for="departement"> filtrer par nom </label>
+
+            <input
+              id="departement"
+              type="text"
+              name="departement"
+              value={inputSearcheValue}
+              onChange={this.handleChangeInput}
+            />
+          </form>
+        </div>
+
+        <table className="liste_departement">
           <tr>
             <th>Code</th>
             <th>Code Region </th>
             <th>Nom </th>
           </tr>
 
-          {data.map((X) => (
+          {departementList.map((X) => (
             <tr key={X.id}>
               <td>{X.code}</td>
               <td>{X.codeRegion}</td>
@@ -42,4 +71,5 @@ class DeppartmentsList extends React.Component {
     );
   }
 }
+
 export default DeppartmentsList;
